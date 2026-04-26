dadec8b2b02d082319a7bfd38d9497969a8396cd
vivekgit-20 <viveksharma20092004@gmail.com>
2026-04-27 02:26:56 +0530

    Time: 3 ms (28.97%) | Memory: 8.5 MB (82.57%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+         int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Overflow check
+            if (rev > INT_MAX/10 || rev < INT_MIN/10)
+                return 0;
+
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+
+        return rev;
+        
+    }
+};
